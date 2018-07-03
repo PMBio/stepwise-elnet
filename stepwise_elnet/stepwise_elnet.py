eecdb39ad48fbe50f47f95484aed2dc8b988edf1
fpcasale <fncpaolo.casale@gmail.com>
2018-07-03 14:19:38 -0400

    stepwise elnet

--- stepwise_elnet/stepwise_elnet.py
+++ stepwise_elnet/stepwise_elnet.py
@@ -1,10 +1,5 @@
 import sys
 sys.path.append('./..')
-from CFG.settings import *
-import include.data as DATA
-from include.utils import matchIDs
-from include.utils import smartDumpDictHdf5
-from include.normalization import regressOut
 from sklearn import cross_validation
 from sklearn.linear_model import ElasticNetCV
 
@@ -16,63 +11,48 @@ import scipy.stats as ST
 import pandas as PD
 import os
 import itertools
+from optparse import OptionParser
 
-def CondElNet(Y,X1,X2=None,opt1=None,opt2=None,cv=None,cv_opt=None,X1star=None,X2star=None):
+def CondElNet(Y_train, X1 , X1star, elnet_som, X2, X2star, elnet_germ):
     """
-    Y:   Nx1 outcome
-    X1:  NxF1 features 
-    X2:  NxF2 features (if specified X2 is fitted on top of Y after regressing out X1)
-    opts1: dictionary of opts for fitting (Y,X1) 
-    opts2: dictionary of opts for fitting (Y,X2) 
-    cv: sklearn cross validation 
-    cv_opt: option for cross validation 
-    """
-    if cv is None:
-        cv = cross_validation.KFold(**cv_opt)
+    Parameters
+    ----------
+    Y:   (`N`, `1`) ndarray
+        Outcome
+    X1:  (`N`, `F1`) ndarray
+        First set of features (train data)
+    X2:  (`N`, `F2`) ndarray
+        Second set of features (train data)
+    X1star:  (`Ns`, `F1`) ndarray
+        First set of features (test data)
+    X2star:  (`Ns`, `F2`) ndarray
+        Second set of features (test data)
+    elnet_som: sklearn model
+        Elastic net for somatic model
+    elnet_germ: sklearn model
+        Elastic net for germline model
 
+    Returns
+    -------
+    Dictionaty with weights and out-of-sample predictions
+    """
     RV = {}
 
-    # fit (Y,X1) 
-    elnet_som = ElasticNetCV(cv=cv,**opt1)
+    # fit (Y,X1)
     elnet_som.fit(X1, Y_train.ravel())
     RV['weights1'] = elnet_som.coef_
-    if X1star is not None:
-        RV['Ystar1'] = elnet_som.predict(X1star)
+    RV['Ystar1'] = elnet_som.predict(X1star)
 
-    # fit (Y,X2) 
-    if X2 is not None:
-        Y_train_r = Y_train.ravel()-elnet_som.predict(X1)
-        elnet_germ = ElasticNetCV(cv=cv,**opt2)
-        elnet_germ.fit(X2, Y_train_r)
-        RV['weights2'] = elnet_germ.coef_
-        if X2star is not None:
-            RV['Ystar2'] = elnet_germ.predict(X2star)
-            RV['Ystar'] = RV['Ystar1']+RV['Ystar2']
+    # fit (Y,X2)
+    Y_train_r = Y_train.ravel()-elnet_som.predict(X1)
+    elnet_germ.fit(X2, Y_train_r)
+    RV['weights2'] = elnet_germ.coef_
+    RV['Ystar2'] = elnet_germ.predict(X2star)
+    RV['Ystar'] = RV['Ystar1']+RV['Ystar2']
 
     return RV
 
-
-if __name__=='__main__':
-
-    # params
-    opt = {}
-    opt['l1_ratio'] = sp.arange(1,11)*.1
-    opt['copy_X'] = False
-    opt['fit_intercept'] = True
-    opt['max_iter'] = 5000
-    opt1 = opt
-    opt2 = opt
-    # alphas som
-    upper = -.01
-    lower = -2.
-    inv = (upper - lower) / 25.
-    opt1['alphas'] = 10.**sp.arange(lower, upper, inv)
-    # alphas germ
-    upper = -.001
-    lower = -1.
-    inv = (upper - lower) / 25.
-    opt2['alphas'] = 10.**sp.arange(lower, upper, inv)
-
+def generate_data():
     # generate data
     N  = 500
     F1 = 100
@@ -80,20 +60,59 @@ if __name__=='__main__':
     Y  = sp.randn(N,1)
     X1 = sp.rand(N,F1)
     X2 = sp.rand(N,F2)
+    return Y, X1, X2
 
-    # for crossvalidation
-    nfolds = 5
-    cv_idx = 0
+if __name__=='__main__':
 
-    # splitting in train and test 
-    seed = 0
-    cv=cross_validation.KFold(n=Y.shape[0],shuffle=True,n_folds=nfolds,random_state=seed)
-    idx_train,idx_test = next(itertools.islice(cv,cv_idx,cv_idx+1))
+    parser = OptionParser()
+    parser.add_option("--seed", dest='seed', type=int, default=0)
+    parser.add_option("--nfolds", dest='nfolds', type=int, default=5)
+    parser.add_option("--fold_i", dest='fold_i', type=int, default=0)
+    parser.add_option("--outfile", dest='outfile', type=str, default="out.h5")
+    (opt, args) = parser.parse_args()
+    opt_dict = vars(opt)
+
+    # fine params of the two elastic net
+    opt_elnet = {}
+    opt_elnet['l1_ratio'] = sp.arange(1,11)*.1
+    opt_elnet['copy_X'] = False
+    opt_elnet['fit_intercept'] = True
+    opt_elnet['max_iter'] = 5000
+    opt_elnet1 = opt_elnet
+    opt_elnet2 = opt_elnet
+    # alphas som
+    upper = -.01
+    lower = -2.
+    inv = (upper - lower) / 25.
+    opt_elnet1['alphas'] = 10.**sp.arange(lower, upper, inv)
+    # alphas germ
+    upper = -.001
+    lower = -1.
+    inv = (upper - lower) / 25.
+    opt_elnet2['alphas'] = 10.**sp.arange(lower, upper, inv)
+
+    # load/generate data
+    Y, X1, X2 = generate_data()
+
+    # splitting in train and test
+    cv = cross_validation.KFold(n=Y.shape[0],shuffle=True,n_folds=opt.nfolds,random_state=opt.seed)
+    idx_train, idx_test = next(itertools.islice(cv,opt.fold_i,opt.fold_i+1))
     Y_train  = Y[idx_train];  Y_test  = Y[idx_test]
     X1_train = X1[idx_train]; X1_test = X1[idx_test]
     X2_train = X2[idx_train]; X2_test = X2[idx_test]
 
-    cv_opt = {'n':Y_train.shape[0],'shuffle':True,'n_folds':nfolds,'random_state':0}
+    # define inner crossvaludation and conditional elnet
+    cv_opt = {'n':Y_train.shape[0],'shuffle':True,'n_folds':opt.nfolds,'random_state':0}
+    cv_inner = cross_validation.KFold(**cv_opt)
+    elnet_som = ElasticNetCV(cv=cv_inner,**opt_elnet1)
+    elnet_germ = ElasticNetCV(cv=cv_inner,**opt_elnet2)
 
-    res = CondElNet(Y_train,X1_train,X2=X2_train,opt1=opt1,opt2=opt2,cv_opt=cv_opt,X1star=X1_test,X2star=X2_test)
+    # run model
+    res = CondElNet(Y_train,X1_train,X1_test,elnet_som,X2_train,X2_test,elnet_germ)
+    res['idxs'] = idx_test
+    res['Y'] = Y_test
 
+    f = h5py.File(opt.outfile, 'w')
+    for key in res.keys():
+        f.create_dataset(key, data=res[key])
+    f.close()
